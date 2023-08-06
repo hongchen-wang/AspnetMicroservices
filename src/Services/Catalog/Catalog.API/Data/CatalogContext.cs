70d9dd0fea60acf64fef2737475c8dbca78a25d8
Hongchen WANG <hongchen.wang@hotmail.fr>
2023-08-06 22:59:00 +0200

    Add Data layer + Business layer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Data.Interfaces;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; set; }
+
+        public CatalogContext(IConfiguration configuration) 
+        {
+            var connectionString = configuration.GetValue<string>("DatabaseSetting:ConnectionString");
+            var client = new MongoClient(connectionString);
+
+            var databaseSetting = configuration.GetValue<string>("DatabaseSetting:DatabaseName");
+            var database = client.GetDatabase(databaseSetting);
+
+            var collectionName = configuration.GetValue<string>("DatabaseSetting:CollectionName");
+            Products = database.GetCollection<Product>(collectionName);
+
+            CatalogContextSeed.SeedData(Products);
+        }
+    }
+}
